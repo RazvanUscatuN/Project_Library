05ae073580d3a45e5da1bde98515a2b8deea3967
Razvan U <129730664+RazvanUscatuN@users.noreply.github.com>
2024-04-15 16:43:32 +0300

    book structure

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello, World");
+
+
+const myLibrary = [];
+
+function Book(title, author, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.readStatus = readStatus;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+
+function removeBookFromLibrary(){
+    //do stuff
+}
